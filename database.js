40e4dc5d2938528f65553f1b1d2c185cfb52a94f
Beers15 <alexbeerz@yahoo.com>
2021-10-24 16:28:06 -0700

    Add start script for Heroku depolyment.

--- database.js
+++ database.js
@@ -18,7 +18,7 @@ const pool = mysql.createPool({
   port: `${process.env.DB_PORT}`,
   user: `${process.env.DB_USER}`,
   password: `${process.env.DB_PASS}`,
-  database: 'Swapper_development'
+  database: `${process.env.DB_NAME}`
 });
 
 pool.getConnection((err, conn) => {
